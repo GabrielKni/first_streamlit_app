50f4e136c843dc218899ec4862b0932f2f13deb8
GabrielKni <39495594+GabrielKni@users.noreply.github.com>
2022-08-09 14:50:52 -0400

    Update streamlit_app.py
    Second step in cleaning up and making this look like a proper app.

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,17 @@ streamlit.dataframe(fruits_to_show)
 
 # New section to display the Fruityvice API response
 streamlit.header("Fruityvice Fruit Advice")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.write('The user entered', fruit_choice)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# Take the json response element and normalize it via Pandas
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# Output the normalized data to a streamlit dataframe (table look)
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 # don't run anything after this point
 streamlit.stop()
