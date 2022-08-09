02142e824bd0f254d1f6b2d89d2b53f02c55b345
GabrielKni <39495594+GabrielKni@users.noreply.github.com>
2022-08-09 14:21:49 -0400

    Update streamlit_app.py
    Added insert in a bad spot with no control.

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,3 +45,6 @@ streamlit.dataframe(my_data_row)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 streamlit.write('Thank you for adding: ', add_my_fruit)
+
+# Comment from the walkthru that really doesn't make sense to put here
+cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from Streamlit');")
